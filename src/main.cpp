8f96037aea03abc8badd82e4e1e4c0fa2ef00e03
dxlogin <xiaochd@gmail.com>
2023-11-08 11:54:09 +0800

    显示主页相关信息

--- src/main.cpp
+++ src/main.cpp
@@ -78,17 +78,30 @@ void parse_homepage_Preferences(const char * pre_path)
 {
    cJSON * root = _chrome_get_json(pre_path);
 
+   cJSON * browser_json = cJSON_GetObjectItem(root,"browser");
+   if(browser_json)
+   {
+      cJSON * show_home_button_json = cJSON_GetObjectItem(browser_json,"show_home_button");
+      printf("[browser.show_home_button] %d\r\n",show_home_button_json->valueint);
+   }
+
    cJSON * hp_json = cJSON_GetObjectItem(root,"homepage");
    if(hp_json)
    {
-      printf("\r\n[Hp] %s\r\n",hp_json->valuestring);
+      printf("[homepage] %s\r\n",hp_json->valuestring);
+   }
+
+   cJSON *homepage_is_newtabpage_json = cJSON_GetObjectItem(root,"homepage_is_newtabpage");
+   if(homepage_is_newtabpage_json)
+   {
+      printf("[homepage_is_newtabpage] %d\r\n",homepage_is_newtabpage_json->valueint);
    }
 
    cJSON * session_json = cJSON_GetObjectItem(root,"session");
    if(session_json)
    {
       cJSON * type_json = cJSON_GetObjectItem(session_json,"restore_on_startup");
-      printf("\r\n[Restore_on_startup] %d\r\n",type_json->valueint);
+      printf("[session.restore_on_startup] %d\r\n",type_json->valueint);
 
       cJSON * startup_urls = cJSON_GetObjectItem(session_json,"startup_urls");
       if(startup_urls)
@@ -96,8 +109,45 @@ void parse_homepage_Preferences(const char * pre_path)
          for(int index = 0; index < cJSON_GetArraySize(startup_urls); index++)
          {
             cJSON * url_json = cJSON_GetArrayItem(startup_urls,index);
-            printf("[startup_url_%d] %s\r\n",index + 1,url_json->valuestring);
+            printf("[session.startup_urls %d] %s\r\n",index + 1,url_json->valuestring);
          }
       }
+   }
+
+   printf("\r\nhmac数据\r\n");
+
+   cJSON * macs_json = cJSON_GetObjectItem(cJSON_GetObjectItem(root,"protection"),"macs");
+
+   cJSON * macs_browser_show_home_button = cJSON_GetObjectItem(cJSON_GetObjectItem(macs_json,"browser"),"show_home_button");
+   printf("[protection.macs.browser.show_home_button] %s\r\n",macs_browser_show_home_button->valuestring);
+
+   cJSON * macs_homepage_json = cJSON_GetObjectItem(macs_json,"homepage");
+   if(macs_homepage_json)
+   {
+      printf("[protection.macs.homepage] %s\r\n",macs_homepage_json->valuestring);
+   }
+
+   cJSON * macs_homepage_is_newtabpage = cJSON_GetObjectItem(macs_json,"homepage_is_newtabpage");
+   if(macs_homepage_is_newtabpage)
+   {
+      printf("[protection.macs.homepage_is_newtabpage] %s\r\n",macs_homepage_is_newtabpage->valuestring);
+   }
+
+   cJSON * macs_session_restore_on_startup = cJSON_GetObjectItem(cJSON_GetObjectItem(macs_json,"session"),"restore_on_startup");
+   if(macs_session_restore_on_startup)
+   {
+      printf("[protection.macs.session.restore_on_startup] %s\r\n",macs_session_restore_on_startup->valuestring);
+   }
+
+   cJSON * macs_session_startup_urls = cJSON_GetObjectItem(cJSON_GetObjectItem(macs_json,"session"),"startup_urls");
+   if(macs_session_startup_urls)
+   {
+      printf("[protection.macs.startup_urls] %s\r\n",macs_session_startup_urls->valuestring);
+   }
+
+   cJSON * super_mac_json = cJSON_GetObjectItem(cJSON_GetObjectItem(root,"protection"),"super_mac");
+   if(super_mac_json)
+   {
+      printf("[protection.super_mac] %s\r\n",super_mac_json->valuestring);
    }
 }
